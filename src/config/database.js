d2e94208c830971433ff46c15259e879a9f4da99
wiinan <wiinan2011@gmail.com>
2021-08-08 19:48:24 -0300

    bugs

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,10 @@
 module.exports = {
     dialect: 'postgres',
     host: 'localhost',
-    username:'postgres',
-    password:'nita99pr',
-    database:'ormtest',
-    port:'3001',
+    username: 'postgres',
+    password: process.env.DB_password,
+    database: 'ormtest',
+    port: '3001',
     define: {
         timestamps: true,
         underscored: true
